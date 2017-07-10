7f8abbf742f2481e436a8d79580084129d4368ca
Dallin Parker <dallin.r.parker@gmail.com>
2017-07-10 15:29:23 -0600

    work

--- drill-4/src/App.js
+++ drill-4/src/App.js
@@ -1,74 +1,85 @@
 import React, { Component } from 'react';
-import { addGuest, removeGuest } from './ducks/guestList';
+import { addGuest, removeGuest, updateName } from './ducks/guestList';
+import EditGuest from './components/EditGuest/EditGuest';
 import { connect } from 'react-redux';
 import './App.css';
 
-
-
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      text: '',
-      edit: false,
-      guestToEdit: '',
-      guestIndex: 0
-    }
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.editName = this.editName.bind(this);
-    this.hideModal = this.hideModal.bind(this);
-  }
+	constructor() {
+		super();
+		this.state = {
+			text: '',
+			edit: false,
+			guestToEdit: '',
+			index: 0
+		}
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.showModal = this.showModal.bind(this);
+		this.hideModal = this.hideModal.bind(this);
+		this.editName = this.editName.bind(this);
+		this.updateGuestName = this.updateGuestName.bind(this)
+	}
 
-  editName(guest, i) {
-    this.setState({
-      edit: true,
-      guestToEdit: guest,
-      guestIndex: i
-    })
-  }
+	handleInputChange(e) {
+		this.setState({
+			text: e.target.value
+		})
+	}
+	handleSubmit(e) {
+		e.preventDefault();
+		this.props.addGuest(this.state.text);
+		this.setState({
+			text: ''
+		})
+	}
 
-  handleInputChange(e) {
-    this.setState({
-      text: e.target.value
-    })
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    this.props.addGuest(this.state.text);
-    this.setState({
-      text: ''
-    })
-  }
+	showModal(guest, index) {
+		this.setState({
+			edit: true,
+			guestToEdit: guest,
+			index: index
+		})
+	}
 
-  hideModal() {
-    this.setState({
-      edit: false
-    })
-  }
+	hideModal() {
+		this.setState({
+			edit: false
+		})
+	}
 
-  render() {
-    return (
+	editName(e) {
+		this.setState({
+			guestToEdit: e.target.value
+		})
+	}
+
+	updateGuestName() {
+		this.props.updateName(this.state.guestToEdit, this.state.index)
+		this.hideModal()
+	}
+
+	render() {
+		return (
       <div className="App">
         <h1>DevMountain Hackathon</h1>
         <h3>Guest List:</h3>
         <ul>
-          {this.props.list.map( (guest, i) => {
-            return (
-              <div key={i} className="list-item">
-                <li>{guest}</li>
-                <div className="">
-                  <button
-                    onClick={() => this.editName(guest, i)}
-                    type="">Edit</button>
-                  <button
-                    onClick={()=> this.props.removeGuest(i)}
-                    type=""
-                    className="">Remove</button>
+					{
+						this.props.list.map( (guest, i) => {
+							return (
+                <div key={i} className="list-item">
+                  <li>{guest}</li>
+                  <div>
+                    <button
+                      onClick={()=>this.showModal( guest, i)}
+                    >Edit</button>
+                    <button onClick={()=> this.props.removeGuest(i)}>Remove</button>
+                  </div>
                 </div>
-              </div>
-            )
-          })}
+							)
+						})
+					}
         </ul>
         <form
           onSubmit={this.handleSubmit}
@@ -76,25 +87,28 @@ class App extends Component {
           Add guest: <input
           value={this.state.text}
           onChange={this.handleInputChange}
-          type="" className=""/>
-          <button
-            type=""
-            className="">Add</button>
+        />
+          <button>Add</button>
         </form>
-        {
-           this.state.edit ?
-                {/* EditGuest */}
-                : null
-        }
+				{
+					this.state.edit ?
+            <EditGuest
+              hide={this.hideModal}
+              guest={this.state.guestToEdit}
+              edit={this.editName}
+              update={this.updateGuestName} />
+						: null
+				}
+
       </div>
-    );
-  }
+		);
+	}
 }
 
 function mapStateToProps(state) {
-  return {
-    list: state.guests
-  }
+	return {
+		list: state.guests
+	}
 }
 
-export default connect(mapStateToProps,{ addGuest, removeGuest })(App);
+export default connect(mapStateToProps,{ addGuest, removeGuest, updateName })(App);
--- drill-4/src/ducks/guestList.js
+++ drill-4/src/ducks/guestList.js
@@ -1,31 +1,49 @@
 const ADD_GUEST = 'ADD_GUEST';
 const REMOVE_GUEST = 'REMOVE_GUEST';
+const UPDATE_GUEST = 'UPDATE_GUEST';
 
 const initialState = {
-  guests: ['Tony Stark', 'Steve Rodgers', ' Nick Fury', 'Natasha Romanova', 'Clint Barton', 'Bruce Banner', 'Wanda Maximoff']
+	guests: ['Tony Stark', 'Steve Rodgers', ' Nick Fury', 'Natasha Romanova', 'Clint Barton', 'Bruce Banner', 'Wanda Maximoff']
 };
 
 export function addGuest(guest) {
-  return {
-    type: ADD_GUEST,
-    payload: guest
-  }
+	return {
+		type: ADD_GUEST,
+		payload: guest
+	}
 }
 
 export function removeGuest(i) {
-  return {
-    type: REMOVE_GUEST,
-    payload: i
-  }
+	return {
+		type: REMOVE_GUEST,
+		payload: i
+	}
+}
+
+export function updateName(name, index) {
+	return {
+		type: UPDATE_GUEST,
+		payload: {
+			name: name,
+			index: index
+		}
+	}
 }
 
 export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case ADD_GUEST:
-      return Object.assign({}, state, {guests: [...state, action.payload]});
-    case REMOVE_GUEST:
-      return Object.assign({}, state, {guests: state.guests.filter((guest, i) => i !== action.payload)});
-    default:
-      return state;
-    }
+	switch (action.type) {
+		case ADD_GUEST:
+			return Object.assign({}, state, {guests: [...state.guests, action.payload]});
+		case REMOVE_GUEST:
+			return Object.assign({}, state, {guests: state.guests.filter((guest, i) => i !== action.payload)});
+		case UPDATE_GUEST:
+			return {
+				guests: state.guests.map((name, i) => {
+					if (action.payload.index === i) return action.payload.name;
+					return name;
+				})
+			}
+		default:
+			return state;
+	}
 }
